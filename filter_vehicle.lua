6b8e7b8a581deae9a458d96455e8e75fc4b85db6
Gvarados <39461072+gvarados1@users.noreply.github.com>
2020-02-13 16:58:04 -0600

    Delete filter_vehicle.lua

--- filter_vehicle.lua
+++
@@ -1,9 +0,0 @@
-// very impressive, lots of code
-ENT.Base = "base_entity"
-ENT.Type = "filter"
-
-function ENT:PassesFilter( trigger, ent )
-	if( IsValid(ent) && ent:IsVehicle() )then
-		return true
-	end
-end
